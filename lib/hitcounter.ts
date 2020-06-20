3080e35361afd6e01f50ad420b30d89d00ef0873
utgwkk <utagawakiki@gmail.com>
2020-06-20 18:23:32 +0900

    add DynamoDB and Lambda resources for hitcounter

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
@@ -7,9 +8,24 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends cdk.Construct {
+  /** allows accessing the counter function */
+  public readonly handler: lambda.Function;
+
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    // TODO
+    const table = new dynamodb.Table(this, 'Hits', {
+        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+        runtime: lambda.Runtime.NODEJS_10_X,
+        handler: 'hitcounter.handler',
+        code: lambda.Code.fromAsset('lambda'),
+        environment: {
+            DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+            HITS_TABLE_NAME: table.tableName
+        }
+    });
   }
 }
