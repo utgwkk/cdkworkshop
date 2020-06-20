5217b96967036cb771b4891a4ac03aeab9455957
utgwkk <utagawakiki@gmail.com>
2020-06-20 19:32:45 +0900

    descending order

--- lib/cdkworkshop-stack.ts
+++ lib/cdkworkshop-stack.ts
@@ -27,7 +27,7 @@ export class CdkworkshopStack extends cdk.Stack {
     new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table,
-      sortBy: 'hits'
+      sortBy: '-hits'
     });
   }
 }
